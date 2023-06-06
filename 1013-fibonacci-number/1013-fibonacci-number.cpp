a674d860422e8b4074c84f4dbeb4b15ddc517069
shrutikaj1098 <47083611+shrutikaj1098@users.noreply.github.com>
2023-06-06 12:39:40 +0530

    Time: 3 ms (68.67%), Space: 5.9 MB (95.01%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -16,10 +16,42 @@ public:
         dp[n]=topdown(n-1,dp)+topdown(n-2,dp);
         return dp[n];
     }
+    int bottomup(int n)
+    {
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        if(n==0) return dp[0];
+        dp[1]=1;
+        if(n==1) return dp[1];
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    int spaco(int n)
+    {
+        int prev2=0;
+        if(n==0) return prev2;
+        int prev1=1;
+        if(n==1) return prev1;
+        int curr;
+        for(int i=2;i<=n;i++)
+        {
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return curr;
+    }
     int fib(int n) {
         // return recsolve(n);
-        //topdown approach:
-        vector<int>dp(n+1,-1);
-        return topdown(n,dp);
+        //topdown approach: recursive memoisation
+        // vector<int>dp(n+1,-1);
+        // return topdown(n,dp);
+        //bottom up approach : iterative tabular method
+        // return bottomup( n);
+        //Space optimization:
+        return spaco(n);
     }
 };
