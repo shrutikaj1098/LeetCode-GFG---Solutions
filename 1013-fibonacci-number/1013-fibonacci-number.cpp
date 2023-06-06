7e8aff199fffd66ebade8ab488d5216e467fd6a0
shrutikaj1098 <47083611+shrutikaj1098@users.noreply.github.com>
2023-06-06 12:33:44 +0530

    Time: 0 ms (100%), Space: 6.1 MB (8.56%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -9,7 +9,17 @@ public:
         int ans=fib(n-1)+fib(n-2);
         return ans;
     }
+    int topdown(int n,vector<int>&dp)
+    {
+        if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=topdown(n-1,dp)+topdown(n-2,dp);
+        return dp[n];
+    }
     int fib(int n) {
-        return recsolve(n);
+        // return recsolve(n);
+        //topdown approach:
+        vector<int>dp(n+1,-1);
+        return topdown(n,dp);
     }
 };
