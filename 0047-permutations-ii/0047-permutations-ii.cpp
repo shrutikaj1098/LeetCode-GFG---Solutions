34a4a5d144a5e239939db6fdafd28cda85d5852c
shrutikaj1098 <47083611+shrutikaj1098@users.noreply.github.com>
2023-06-07 19:10:34 +0530

    Time: 11 ms (59.54%), Space: 11.8 MB (20.66%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -8,9 +8,11 @@ public:
             ans.push_back(nums);
             return;
         }
-
+        unordered_map<int,bool>visited;
         for(int i=start;i<nums.size();i++)
         {
+            if(visited.find(nums[i])!=visited.end()) continue;
+            visited[nums[i]]=true;
             swap(nums[i],nums[start]);
             permutehelper(nums,ans,start+1);
             swap(nums[i],nums[start]);
@@ -20,16 +22,16 @@ public:
          vector<vector<int>>ans;
         // vector<int>v;
         permutehelper(nums,ans,0);
-        set<vector<int>>s;
-        for(auto i:ans)
-        {
-            s.insert(i);
-        }
-        ans.clear();
-        for(auto i:s)
-        {
-            ans.push_back(i);
-        }
+        // set<vector<int>>s;
+        // for(auto i:ans)
+        // {
+        //     s.insert(i);
+        // }
+        // ans.clear();
+        // for(auto i:s)
+        // {
+        //     ans.push_back(i);
+        // }
         return ans;
 
     }
