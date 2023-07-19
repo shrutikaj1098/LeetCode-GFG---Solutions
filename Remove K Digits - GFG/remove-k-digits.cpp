51d19a864b8d082284934dfb14f09e4cae0c0f26
shrutikaj1098 <47083611+shrutikaj1098@users.noreply.github.com>
2023-07-19 16:06:52 +0530

    Added solution - LeetHub

---
+++ Remove K Digits - GFG/remove-k-digits.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string removeKdigits(string num, int k) {
+         string ans="";
+        for(char &c:num)
+        {
+            while(ans.size() && ans.back()>c &&k)
+            {
+                ans.pop_back();
+                k--;
+            }
+            if(ans.size()||c!='0')ans.push_back(c);
+        }
+        while(ans.size()&&k--)           
+        {
+            ans.pop_back();
+        }
+        return (ans=="")?"0":ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        int K;
+        cin >> K;
+        Solution obj;
+        cout << obj.removeKdigits(S, K) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
